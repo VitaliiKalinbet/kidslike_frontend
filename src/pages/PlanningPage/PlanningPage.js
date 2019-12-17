7b4b0192d6a779bbbe7f2212de259b5207040d5a
Yaroslav <kovshun.yv@gmail.com>
2019-12-18 00:34:20 +0200

    add layout PlanningPage, PlanningPoints, AddCustomTask

--- src/pages/PlanningPage/PlanningPage.js
+++ src/pages/PlanningPage/PlanningPage.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import styles from './Planning.module.css';
+import PlanningPoints from '../../components/PlanningPage/PlanningPoints';
+import AddCustomTask from '../../components/PlanningPage/AddCustomTask';
 
 const PlanningPage = () => {
-  return <div>PlanningPage</div>;
+  return (
+    <div className={styles.planningContainer}>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>First</li>
+        <li className={styles.listItem}>Second</li>
+        <li className={styles.listItem}>Third</li>
+        <li className={styles.listItem}>Fourth</li>
+        <li className={styles.listItem}>First</li>
+        <li className={styles.listItem}>Second</li>
+        <li className={styles.listItem}>Third</li>
+        <li className={styles.listItem}>Fourth</li>
+      </ul>
+      <div className={styles.pointsTaskContainer}>
+        <AddCustomTask />
+        <PlanningPoints />
+      </div>
+    </div>
+  );
 };
 
 export default PlanningPage;
