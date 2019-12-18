dc511b3baa6e46e22d4cd02222ccbae9f21fca52
SuminDV <sumin.dmytro@gmail.com>
2019-12-18 12:34:39 +0200

    DaysList added

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Progress } from 'react-sweet-progress';
-import PropTypes from 'prop-types';
 import style from './ProgressBar.module.css';
 import 'react-sweet-progress/lib/style.css';
 
-const ProgressBar = ({ points, allPoints }) => {
+const ProgressBar = () => {
   return (
     <>
-      <p>Балів з початку тижня</p>
-      <p className={style.text}>
-        {points}/{allPoints}
-      </p>
       <div className={style.Progress}>
         <Progress percent={88} />
       </div>
     </>
   );
-};
-
-ProgressBar.propTypes = {
-  points: PropTypes.number.isRequired,
-  allPoints: PropTypes.number.isRequired,
 };
 
 export default ProgressBar;
