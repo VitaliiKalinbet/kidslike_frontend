073b68cadb415da53e87e3d964b4846ece3ed45f
SuminDV <sumin.dmytro@gmail.com>
2019-12-18 17:56:52 +0200

    Daylist done with Svg

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import DaysList from '../../components/DaysList/DaysList';
+import daysJson from '../../components/DaysList/DaysList.json';
 
 const MainPage = () => {
-  return <div />;
+  return (
+    <div>
+      <DaysList daysJson={daysJson} />
+    </div>
+  );
 };
 
 export default MainPage;
