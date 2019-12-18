c57e1764af86271846ec5bdc525b12e7b3f9f176
Yaroslav <kovshun.yv@gmail.com>
2019-12-18 11:00:58 +0200

    some edit styles

--- src/pages/PlanningPage/PlanningPage.js
+++ src/pages/PlanningPage/PlanningPage.js
@@ -6,19 +6,21 @@ import AddCustomTask from '../../components/PlanningPage/AddCustomTask';
 const PlanningPage = () => {
   return (
     <div className={styles.planningContainer}>
-      <ul className={styles.list}>
-        <li className={styles.listItem}>First</li>
-        <li className={styles.listItem}>Second</li>
-        <li className={styles.listItem}>Third</li>
-        <li className={styles.listItem}>Fourth</li>
-        <li className={styles.listItem}>First</li>
-        <li className={styles.listItem}>Second</li>
-        <li className={styles.listItem}>Third</li>
-        <li className={styles.listItem}>Fourth</li>
-      </ul>
-      <div className={styles.pointsTaskContainer}>
-        <AddCustomTask />
-        <PlanningPoints />
+      <div className={styles.planningWrapper}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>First</li>
+          <li className={styles.listItem}>Second</li>
+          <li className={styles.listItem}>Third</li>
+          <li className={styles.listItem}>Fourth</li>
+          <li className={styles.listItem}>First</li>
+          <li className={styles.listItem}>Second</li>
+          <li className={styles.listItem}>Third</li>
+          <li className={styles.listItem}>Fourth</li>
+        </ul>
+        <div className={styles.pointsTaskContainer}>
+          <AddCustomTask />
+          <PlanningPoints />
+        </div>
       </div>
     </div>
   );
