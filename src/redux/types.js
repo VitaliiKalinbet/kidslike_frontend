6e9b9d179ab3ade7da099652cde4463c9bf30313
Seymur <mustafav.seymur@gmail.com>
2019-12-18 16:45:39 +0200

    LoaderLogic

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,5 +1,9 @@
 const types = {
   START_REGISTER: 'START_REGISTER',
+  // ----------Loader----------------------------
+
+  APP_FETCHING_START: 'APP_FETCHING_START',
+  APP_FETCHING_SUCCESS: ' APP_FETCHING_SUCCESS',
 };
 
 export default types;
