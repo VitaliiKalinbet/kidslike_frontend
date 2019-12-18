772ee3bffb254f21e602ab3b1defd466bc5312e3
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-12-18 13:22:58 +0200

    AuthPage

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './AuthPage.module.css';
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <>
+      <div className={styles.bck__color}>
+        <div className={styles.auth__bck}>
+          <div className={styles.container}>
+            <h2 className={styles.auth__title}>
+              Виконуй завдання, отримай класні призи!
+            </h2>
+            <div className={styles.auth__reg}>Registration</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AuthPage;
