44bfb03fce365455ab00f47273dc99f499254a45
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-18 14:17:38 +0200

    added members photos, changed styles css

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -1,83 +1,32 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import styles from './ContactsPages.module.css';
-import ContactPhoto from '../../assets/images/contact-photo.png';
+import Contacts from '../../utils/contactsInfo.js';
 
 const ContactsPage = () => {
   return (
     <div className={styles.contacts}>
-      <p className={styles.contacts_main_tittle}>Наша команда</p>{' '}
+      <p className={styles.contacts_main_tittle}>Наша команда</p>
       <p className={styles.contacts_second_tittle}>
         Завжди готові до нових викликів!
       </p>
       <ul className={styles.contacts_all_card}>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img
-            className={styles.contacts_img}
-            src={ContactPhoto}
-            alt="team_photo"
-          />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
-        <li className={styles.contacts_card}>
-          <img className={styles.contacts_img} src={ContactPhoto} alt="" />
-          <p className={styles.contacts_name}>Name and Surname</p>
-          <p className={styles.contacts_possition}>Front-End Developer</p>
-          <p className={styles.contacts_possition_describe}>
-            Lorem Ipsum является стандартной для текстов на латиниц
-          </p>
-        </li>
+        {Contacts.map(el => (
+          <li className={styles.contacts_card} key={el.id}>
+            <img
+              className={styles.contacts_img}
+              src={el.avatar}
+              alt=""
+              width="280"
+              height="245"
+            />
+            <p className={styles.contacts_name}>{el.name}</p>
+            <p className={styles.contacts_possition}>{el.position}</p>
+            <p className={styles.contacts_possition_describe}>
+              {el.description}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
