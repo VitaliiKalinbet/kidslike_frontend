ac69e7b15ff20f171f7956a44b371f334255b20c
Igor <o.igorok@gmail.com>
2019-12-19 16:38:20 +0200

    css transition

--- src/components/CardsList/index.js
+++ src/components/CardsList/index.js
@@ -1,25 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Card from '../Card';
 import s from './CardsList.module.css';
-import popTransition from './pop.module.css';
 import tasks from './tasks';
 
 const CardsList = () => {
   return (
-    <TransitionGroup component="ul" className={s.cards_list}>
+    <ul className={s.cards_list}>
       {tasks.map(el => {
         const { id, ...task } = el;
         return (
-          <CSSTransition key={id} timeout={5000} classNames={popTransition}>
-            <li className={s.card}>
-              <Card {...task} />
-            </li>
-          </CSSTransition>
+          <li key={id} className={s.card}>
+            <Card {...task} />
+          </li>
         );
       })}
-    </TransitionGroup>
+    </ul>
   );
 };
 
