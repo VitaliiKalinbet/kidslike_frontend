84ca6656af8c0fdf4a0ec607a4c2bd6a41f28194
kievtechnoshop@gmail.com <swr83947>
2019-12-19 16:46:43 +0200

    Modal

---
+++ src/components/CongratsModal/CongratsModal.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-responsive-modal';
+import * as styleCongratsModal from './CongratsModal.module.css';
+import prizeImage1 from '../../assets/images/AwardsPage/1.png';
+import prizeImage2 from '../../assets/images/AwardsPage/2.png';
+import catTop from '../../assets/images/AwardsPage/catTransparent.png';
+
+export default class App extends React.Component {
+  state = {
+    open: false,
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div>
+        <button onClick={this.onOpenModal}>Open modal</button>
+        <Modal open={open} onClose={this.onCloseModal} center>
+          <div className={styleCongratsModal.modalPrizeSize}>
+            <img className={styleCongratsModal.catTop} src={catTop} alt="" />
+            <h2 className={styleCongratsModal.modalTitleText}>
+              Вітаємо! Ти отримуєш:
+            </h2>
+            <div className={styleCongratsModal.prizeContainer}>
+              {/* item open */}
+
+              <div className={styleCongratsModal.prizeContainerItem}>
+                <img
+                  className={styleCongratsModal.imagePrize}
+                  src={prizeImage1}
+                  alt=""
+                />
+                <p className={styleCongratsModal.prizeContainerItemText}>
+                  75 балів
+                </p>
+              </div>
+              {/* item close */}
+              {/* item open */}
+
+              <div className={styleCongratsModal.prizeContainerItem}>
+                <img
+                  className={styleCongratsModal.imagePrize}
+                  src={prizeImage2}
+                  alt=""
+                />
+                <p className={styleCongratsModal.prizeContainerItemText}>
+                  75 балів
+                </p>
+              </div>
+
+              {/* item close */}
+            </div>
+            <div className={styleCongratsModal.buttonContainer}>
+              <button className={styleCongratsModal.button}>button</button>
+            </div>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
