46e2debd63c21ac1fb659d9121e7c75523b0b57d
Igor <o.igorok@gmail.com>
2019-12-20 12:15:22 +0200

    render element done

--- src/components/CardControl/CardControl.js
+++ src/components/CardControl/CardControl.js
@@ -1,56 +1,30 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import moment from 'moment';
-import SelectDays from '../SelectDays/SelectDays';
-import TaskToggle from '../TaskToggle/TaskToggle';
+// import React from 'react';
 
-const CardControl = () => {
-  const { search, pathname } = useLocation();
+// const CardControl = () => {
+//   // baseUrl?day=monday&unix-date=1576503044283
 
-  const today = moment().day();
-  // baseUrl?day=monday&unix-date=1576503044283
-  const urlDay = new URLSearchParams(search).get('day');
-  const url = moment()
-    .day(urlDay)
-    .weekday();
+//   // const today =
+//   //   moment()
+//   //     .date()
+//   //     .toString() +
+//   //   (moment().month() + 1).toString() +
+//   //   moment()
+//   //     .year()
+//   //     .toString();
 
-  // const today =
-  //   moment()
-  //     .date()
-  //     .toString() +
-  //   (moment().month() + 1).toString() +
-  //   moment()
-  //     .year()
-  //     .toString();
+//   // const urlDay =
+//   //   moment(urlDate)
+//   //     .date()
+//   //     .toString() +
+//   //   (moment(urlDate).month() + 1).toString() +
+//   //   moment(urlDate)
+//   //     .year()
+//   //     .toString();
 
-  // const urlDay =
-  //   moment(urlDate)
-  //     .date()
-  //     .toString() +
-  //   (moment(urlDate).month() + 1).toString() +
-  //   moment(urlDate)
-  //     .year()
-  //     .toString();
+//   return renderElement() || null;
+// };
 
-  const renderElement = () => {
-    if (pathname === '/planning') {
-      return <SelectDays />;
-    }
-    if (Number(today) === urlDay) {
-      return <TaskToggle />;
-    }
-    return null;
-  };
-
-  return (
-    console.log('today :', today) ||
-    console.log('url :', url) ||
-    console.log('test :', test) ||
-    renderElement()
-  );
-};
-
-export default CardControl;
+// export default CardControl;
 
 // logic
 
