860307a08d02f1a1a9713b2b5c21eaad4b68d4d3
Andriy Medvedyuk <Andriy.Medvedyuk@gmail.com>
2019-12-23 11:35:07 +0200

    UserInfo

---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './UserInfo.module.css';
+import logo from '../../assets/images/imageRobot.png';
+
+const UserInfo = () => (
+  <div>
+    <span className={styles.box}>
+      <img className={styles.logo} alt="logo" src={logo} />
+      <p className={styles.name}>Ваня</p>
+      <button type="button">Выйти </button>
+    </span>
+  </div>
+);
+
+export default UserInfo;
