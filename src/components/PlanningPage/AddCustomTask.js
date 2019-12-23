dd58438dd52971fdd133fbf73a384404d0382d87
Yaroslav <kovshun.yv@gmail.com>
2019-12-23 16:47:00 +0200

    add logic open modal in AddCustomTask

--- src/components/PlanningPage/AddCustomTask.js
+++ src/components/PlanningPage/AddCustomTask.js
@@ -1,36 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { NewTaskModalOpen } from '../../redux/global/globalActions';
 import styles from '../../pages/PlanningPage/Planning.module.css';
 import NewTaskModal from '../NewTaskModal/NewTaskModal';
 
-export default class AddCustomTask extends Component {
-  state = {
-    isOpenModal: false,
-  };
+// export default class AddCustomTask extends Component {
+//   state = {
+//     isOpenModal: false,
+//   };
 
-  openModal = () => this.setState({ isOpenModal: true });
+//   openModal = () => this.setState({ isOpenModal: true });
 
-  closeModal = () => this.setState({ isOpenModal: false });
+//   closeModal = () => this.setState({ isOpenModal: false });
 
-  render() {
-    const { isOpenModal } = this.state;
+//   render() {
+//     const { isOpenModal } = this.state;
 
-    return (
-      <>
-        <p className={styles.hiddenText}>
-          Хочеш отримати більше призів - додай завдання
-          <span role="img" aria-label="	SMILING FACE WITH SMILING EYES">
-            &#128522;
-          </span>
-        </p>
-        <button
-          className={styles.addTaskBtn}
-          type="button"
-          onClick={this.openModal}
-        >
-          &#43;
-        </button>
-        {isOpenModal && <NewTaskModal onClose={this.closeModal} />}
-      </>
-    );
-  }
-}
+//     return (
+//       <>
+//         <p className={styles.hiddenText}>
+//           Хочеш отримати більше призів - додай завдання
+//           <span role="img" aria-label="	SMILING FACE WITH SMILING EYES">
+//             &#128522;
+//           </span>
+//         </p>
+//         <button
+//           className={styles.addTaskBtn}
+//           type="button"
+//           onClick={this.openModal}
+//         >
+//           &#43;
+//         </button>
+//         {isOpenModal && <NewTaskModal onClose={this.closeModal} />}
+//       </>
+//     );
+//   }
+// }
+
+// eslint-disable-next-line react/prop-types
+const AddCustomTask = ({ isOpenModal, openModal }) => {
+  return (
+    <>
+      <p className={styles.hiddenText}>
+        Хочеш отримати більше призів - додай завдання
+        <span role="img" aria-label="	SMILING FACE WITH SMILING EYES">
+          &#128522;
+        </span>
+      </p>
+      <button className={styles.addTaskBtn} type="button" onClick={openModal}>
+        &#43;
+      </button>
+      {isOpenModal && <NewTaskModal />}
+    </>
+  );
+};
+
+const MSTP = store => ({
+  isOpenModal: store.global,
+});
+
+const MDTP = dispatch => ({
+  openModal: () => dispatch(NewTaskModalOpen),
+});
+
+console.log(MDTP);
+
+export default connect(MSTP, MDTP)(AddCustomTask);
