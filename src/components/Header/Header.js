ba57e112bae09202a5f6016be9a58b772f421081
sserichenko <sserichenko@gmail.com>
2019-12-23 13:23:43 +0200

    import UserInfo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import * as moment from 'moment';
 import slideTransition from '../../transitions/fade.module.css';
 import Navigation from '../Navigation/Navigation';
 import HeaderModal from '../HeaderModal/HeaderModal';
+import UserInfo from '../UserInfo/UserInfo';
 import styles from './Header.module.css';
 import { ReactComponent as Logo } from '../../assets/icons/header-icons/burger.svg';
 import logoMobile from '../../assets/icons/header-icons/Logo_mobile.png';
@@ -52,18 +53,7 @@ class Header extends Component {
           <div className={styles.navControls}>
             {!isAuth && <Navigation />}
             <div className={styles.authModule}>
-              {/* <img className={styles.userLogo} alt="Zagl" src={Zaglushka} />
-              <p className={styles.userName}>Ваня</p>
-              {isAuth && (
-                <button
-                  onClick={this.openLogOutModal}
-                  type="button"
-                  className={styles.buttonLogout}
-                >
-                  Вийти
-                </button>
-              )} */}
-
+              {!isAuth && <UserInfo />}
               <button
                 onClick={this.openModal}
                 className={styles.button}
