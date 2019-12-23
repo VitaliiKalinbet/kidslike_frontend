84813007b8d6f7e8210a2c00df0867ef1c211bb1
sserichenko <sserichenko@gmail.com>
2019-12-23 11:45:13 +0200

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,10 @@ import * as moment from 'moment';
 import slideTransition from '../../transitions/fade.module.css';
 import Navigation from '../Navigation/Navigation';
 import HeaderModal from '../HeaderModal/HeaderModal';
-// import ModalLogout from '../';
 import styles from './Header.module.css';
 import { ReactComponent as Logo } from '../../assets/icons/header-icons/burger.svg';
 import logoMobile from '../../assets/icons/header-icons/Logo_mobile.png';
-import Zaglushka from '../../assets/icons/header-icons/Zaglushka.jpg';
+// import Zaglushka from '../../assets/icons/header-icons/Zaglushka.jpg';
 
 class Header extends Component {
   static propTypes = {
@@ -20,28 +19,19 @@ class Header extends Component {
 
   state = {
     isModalOpen: false,
-    // isLogOutModal: false,
   };
 
   openModal = () => this.setState({ isModalOpen: true });
 
   closeModal = () => this.setState({ isModalOpen: false });
 
-  // openLogOutModal = () => this.setState({ isLogOutModal: true });
-
-  // closeLogOutModal = () => this.setState({ isLogOutModal: false });
-
   render() {
     const { isModalOpen } = this.state;
-    // const { isLogOutModal } = this.state;
     const unixDate = Date.now();
     const currentDay = moment().format('dddd');
     const { isAuth } = this.props;
     return (
       <>
-        {/* {isLogOutModal && (
-          <ModalLogout closeLogOutModal={this.closeLogOutModal} />
-        )} */}
         <CSSTransition
           in={isModalOpen}
           timeout={1000}
@@ -60,9 +50,9 @@ class Header extends Component {
             <img className={styles.siteLogo} alt="SiteLogo" src={logoMobile} />
           </Link>
           <div className={styles.navControls}>
-            <Navigation />
+            {!isAuth && <Navigation />}
             <div className={styles.authModule}>
-              <img className={styles.userLogo} alt="Zagl" src={Zaglushka} />
+              {/* <img className={styles.userLogo} alt="Zagl" src={Zaglushka} />
               <p className={styles.userName}>Ваня</p>
               {isAuth && (
                 <button
@@ -72,7 +62,7 @@ class Header extends Component {
                 >
                   Вийти
                 </button>
-              )}
+              )} */}
 
               <button
                 onClick={this.openModal}
