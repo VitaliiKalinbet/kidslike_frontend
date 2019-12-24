e6400c126a6686b3aad6363c51a204adaa8db6d8
Yaroslav <kovshun.yv@gmail.com>
2019-12-24 13:21:25 +0200

    add in folder utils tasks.json and totalWeekPlanPoints.js

--- src/components/PlanningPage/PlanningPoints.js
+++ src/components/PlanningPage/PlanningPoints.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import propTypes from 'prop-types';
 import styles from '../../pages/PlanningPage/Planning.module.css';
-import { getTaskPoints } from '../../redux/tasks/tasksSelector';
 
-const PlanningPoints = ({ taskPoints }) => (
+const PlanningPoints = () => (
   <p className={styles.pointsText}>
     <span className={styles.hiddenSpan}>Визначено завдань на</span>
-    <span className={styles.pointsAmount}>{taskPoints}</span>
+    <span className={styles.pointsAmount}>0</span>
     бали
   </p>
 );
 
-PlanningPoints.propTypes = {
-  taskPoints: propTypes.number.isRequired,
-};
-
-const MSTP = state => ({
-  taskPoints: getTaskPoints(state),
-});
-
-export default connect(MSTP)(PlanningPoints);
+export default PlanningPoints;
--- src/redux/tasks/tasksSelector.js
+++
@@ -1,4 +0,0 @@
-// import { createSelector } from 'reselect';
-
-export const getActiveDays = store => store.tasks.days.isActive;
-export const getTaskPoints = store => store.tasks.taskPoints;
