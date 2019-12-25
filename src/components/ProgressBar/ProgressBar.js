a1f90f209b13e45adeaec4f9e7d40033fc96c61d
SuminDV <sumin.dmytro@gmail.com>
2019-12-25 13:58:45 +0200

    updating progressbar, need to complite

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,22 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Progress } from 'react-sweet-progress';
-import style from './ProgressBar.module.css';
 import 'react-sweet-progress/lib/style.css';
+import PropTypes from 'prop-types';
+import style from './ProgressBar.module.css';
 
-const ProgressBar = () => {
+const ProgressBar = ({ userPoints, weekPoints }) => {
+  const points = { userPoints };
+  const pointsMax = { weekPoints };
+
+  const percent = (points / pointsMax) * 100;
+
   return (
     <>
       <div className={style.Progress}>
         <p className={style.progressText}>Набрано балiв: </p>
         <div className={style.progressBarDiv}>
           <p className={style.progressNumbers}>
-            <span>0 / 0</span>
+            <span>{userPoints}</span>
           </p>
-          <Progress percent={0} />
+          {<Progress percent={points} /> || <Progress percent={percent} />}
         </div>
       </div>
     </>
   );
 };
 
-export default ProgressBar;
+ProgressBar.propTypes = {
+  userPoints: PropTypes.number.isRequired,
+  weekPoints: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = state => ({
+  userPoints: state.auth.user.points,
+});
+
+export default connect(mapStateToProps, null)(ProgressBar);
