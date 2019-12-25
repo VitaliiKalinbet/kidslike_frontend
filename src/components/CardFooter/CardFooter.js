54942c05c79c2946b1a54cb56f0085e657214c40
Kristina-zh <kristina.zhuravleva1991@gmail.com>
2019-12-25 15:55:41 +0200

    add logic for toggleOpperation

--- src/components/CardFooter/CardFooter.js
+++ src/components/CardFooter/CardFooter.js
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import moment from 'moment';
 import { sumAwardsCardAction } from '../../redux/awards/awardsAction';
-import { changeTaskTodayAction } from '../../redux/tasks/tasksActions';
+import { changeTaskTodayOperation } from '../../redux/tasks/tasksOperations';
 import PointAmount from '../PointAmount/PointAmount';
 import CardTitle from '../CardTitle/CardTitle';
 import SelectDays from '../SelectDays/SelectDays';
@@ -30,7 +30,7 @@ const CardFooter = ({ ...taskInfo }) => {
   };
 
   const handleChangeTaskToday = (e, taskId) => {
-    dispatch(changeTaskTodayAction(taskId, new Date(1577272964056).getDay()));
+    dispatch(changeTaskTodayOperation(taskId));
   };
 
   const renderElement = () => {
