b00dc7e6836af9f17c0b65b7c6fecea47027eb24
Yaroslav <kovshun.yv@gmail.com>
2019-12-26 13:36:59 +0200

    change all styles for PlanninPage and child componentsa and create new component for mobileStyles

--- src/pages/PlanningPage/PlanningPage.js
+++ src/pages/PlanningPage/PlanningPage.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import windowWidth from '../../utils/windowWidth';
 import styles from './Planning.module.css';
+import mobileStyles from './PlanningMobile.module.css';
 import PlanningPoints from '../../components/PlanningPage/PlanningPoints';
+import MobilePlanningPoints from '../../components/PlanningPage/MobilePlanningPoints';
 import ContainerList from '../../components/CardsList';
 import CurrentWeekPlaning from '../../components/CurrentWeekPlaning/CurrentWeekPlaning';
+import MobileCurrentWeekPlaning from '../../components/CurrentWeekPlaning/MobileCurrentWeekPlanning';
 import AddCustomTask from '../../components/PlanningPage/AddCustomTask';
+import MobileAddCustomTask from '../../components/PlanningPage/MobileAddCustomTask';
 
 const PlanningPage = () => {
   const tasks = useSelector(state => state.tasks.items);
 
   return (
     <div className={styles.planningContainer}>
-      <CurrentWeekPlaning />
+      {windowWidth < 768 && <MobileCurrentWeekPlaning />}
+      {windowWidth >= 768 && (
+        <div className={styles.pointsTaskContainer}>
+          <CurrentWeekPlaning />
+          <div className={styles.secondTaskContainer}>
+            <>
+              <PlanningPoints />
+              <AddCustomTask />
+            </>
+          </div>
+        </div>
+      )}
       <div className={styles.list}>
         {tasks && <ContainerList arr={tasks} />}
       </div>
-      <div className={styles.pointsTaskContainer}>
-        <AddCustomTask />
-        <PlanningPoints />
-      </div>
+      {windowWidth < 768 && (
+        <div className={mobileStyles.MobilePointsTaskContainer}>
+          <MobilePlanningPoints />
+          <MobileAddCustomTask />
+        </div>
+      )}
     </div>
   );
 };
